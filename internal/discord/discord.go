aa1b1d876db512da2c0cb6f66aa049a9cb174fc6
Dean Scarff <dos@scarff.id.au>
2021-12-24 01:17:07 +1100

    Add Discord IPC client

---
+++ internal/discord/discord.go
@@ -0,0 +1,260 @@
+// Package discord is an interface to Discord's IPC.
+//
+// Discord exposes an IPC interface over a UNIX domain socket; this is
+// documented (with a reference implementation) at:
+// https://github.com/discord/discord-rpc/blob/master/documentation/hard-mode.md
+//
+// Discord's IPC messages consist of a header and payload.  The header contains
+// an opcode and the length of the payload.
+//
+// The opcode is handled automatically. The first message sent will be assigned
+// a Handshake opcode. Subsequent messages will be assigned the Frame opcode.
+// Pings and close messages (originating from Discord) are handled internally.
+package discord
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+// Client is a wrapper for reading and writing to the Discord client via its
+// IPC socket.
+//
+// Outgoing messages can be queued with Send().  They will be processed,
+// one at a time, once Start() has been called.
+type Client struct {
+	// out receives payloads that should be sent to Discord.
+	//
+	// out is read exclusively by the Start() goroutine, and written to
+	// exclusively by Send() and Close().
+	out chan string
+
+	// in receives messages that were read from Discord.
+	//
+	// in is written to exclusively by the Start() goroutine, and read
+	// exclusively by Send().
+	in chan message
+
+	// conn is the Discord IPC socket.
+	//
+	// conn is accessed exclusively by the Start() goroutine.
+	conn net.Conn
+
+	// Blocks until the last message sent with Send() has received an answer.
+	waitForAnswer chan int
+}
+
+// Discord-RPC message opcodes.
+const (
+	Handshake = 0
+	Frame     = 1
+	Close     = 2
+	Ping      = 3
+	Pong      = 4
+)
+
+// Message is the Discord RPC message.
+//
+// The wire format consists of the opcode, payload length, and payload.
+// Here, the length is inferred from the payload string.
+type message struct {
+	// The Discord-RPC opcode.
+	Opcode int32
+
+	// Payload is the JSON string encoded as UTF-8.
+	Payload string
+}
+
+// getDiscordSocket constructs a path to a Discord IPC socket.
+//
+// A socket may not actually exist at the returned path.
+func getDiscordSocket(tmpDir string, n int) string {
+	return fmt.Sprintf("%s/discord-ipc-%d", tmpDir, n)
+}
+
+// newClient creates a Client with the specified IPC socket.
+func newClient(conn net.Conn) *Client {
+	return &Client{
+		// A buffer size of 1 causes Send() to block until the previous message
+		// has an answer.
+		out:  make(chan string, 1),
+		in:   make(chan message),
+		conn: conn,
+	}
+}
+
+// Dial opens the Discord socket and returns a client for sending messages.
+func Dial(tmpDir string) (c *Client, err error) {
+	var conn net.Conn
+
+	// Socket may be numbered from 0 to 9.
+	for i := 0; i < 10; i++ {
+		addr := getDiscordSocket(tmpDir, i)
+
+		// Go's "unix" network is equivalent to AF_UNIX/SOCK_STREAM.
+		conn, err = net.Dial("unix", addr)
+
+		if err == nil {
+			break
+		}
+	}
+
+	if err != nil {
+		return
+	}
+
+	c = newClient(conn)
+	return
+}
+
+// Start waits for messages written with Send(), and sends them to the socket.
+// It will also listen for any messages initiated by Discord.
+func (c *Client) Start() (err error) {
+	// nextOpcode is the opcode to use for the next packet sent via Send().
+	// The first packet sent will be marked as a Handshake, and subsequent packets
+	// will be marked as Frames.
+	var nextOpcode int32 = Handshake
+
+	// We want to handle events coming from either direction (reads from Discord
+	// or writes from Send()).
+	//
+	// In an ideal world, we'd be able to do a kernel-style select/epoll/kqueue to
+	// multiplex waiting for either event.  Unfortunately in the Go stdlib,
+	// net.Conn only exposes a blocking read, and we don't want two goroutines
+	// accessing the Conn concurrently, so we poll once a second.
+
+EventLoop:
+	for err == nil {
+		// Wait on a Send() or Close().
+		didSend := false
+		select {
+		case payload, ok := <-c.out:
+			if !ok {
+				break EventLoop
+			}
+
+			if err = writeMessage(message{Opcode: nextOpcode, Payload: payload}, c.conn); err != nil {
+				break EventLoop
+			}
+
+			nextOpcode = Frame
+			didSend = true
+		case <-time.After(time.Second):
+		}
+
+		// Try to read from the socket.
+		if didSend {
+			// Block on an answer from Discord.
+			c.conn.SetReadDeadline(time.Time{})
+			var m message
+			m, err = readMessage(c.conn)
+			if err == nil {
+				c.in <- m
+				continue
+			}
+		} else {
+			// Set a negligible deadline to check if there are server-initiated
+			// messages.
+			c.conn.SetReadDeadline(time.Now().Add(time.Millisecond))
+			var m message
+			m, err = readMessage(c.conn)
+			switch {
+			case errors.Is(err, os.ErrDeadlineExceeded):
+				// Normal case - no message from server.
+				err = nil
+			case err != nil:
+				// Terminates loop.
+			case m.Opcode == Ping:
+				// Respond immediately to ping.
+				m.Opcode = Pong
+				err = writeMessage(m, c.conn)
+			case m.Opcode == Close:
+				err = fmt.Errorf("Discord IPC connection terminated by Discord")
+			default:
+				err = fmt.Errorf("Got unexpected opcode: %d, payload: %v", m.Opcode, m.Payload)
+			}
+		}
+	}
+	close(c.in)
+	c.conn.Close()
+	c.conn = nil
+	return
+}
+
+// Send sends the given payload to Discord and returns the answer payload. It
+// blocks on both waiting to send the message, and waiting for the answer to be
+// received.
+//
+// The returned answer does not include the header used by Discord's IPC
+// protocol; it's just the payload (typically JSON).
+func (c *Client) Send(payload string) (ans string, err error) {
+	c.out <- payload
+
+	if m, ok := <-c.in; !ok {
+		return ans, fmt.Errorf("Socket closed while waiting for response")
+	} else {
+		return m.Payload, nil
+	}
+}
+
+// Close terminates the connection to the Discord socket.
+//
+// Send() calls made after Close() will have errors.
+func (c *Client) Close() {
+	close(c.out)
+}
+
+// headerLen is the number of bytes in the Discord IPC message header.
+const headerLen = 8
+
+// encode serializes the message in wire format.
+func (m message) encode() []byte {
+	buf := bytes.NewBuffer(make([]byte, 0, len(m.Payload)+headerLen))
+	binary.Write(buf, binary.LittleEndian, m.Opcode)
+	binary.Write(buf, binary.LittleEndian, int32(len(m.Payload)))
+	buf.WriteString(m.Payload)
+	return buf.Bytes()
+}
+
+// readMessage reads a message from the socket.
+func readMessage(conn net.Conn) (m message, err error) {
+	header := make([]byte, headerLen)
+	var n int
+	switch n, err = conn.Read(header); {
+	case err != nil:
+		return
+	case n != headerLen:
+		err = fmt.Errorf("Wanted %d-byte header, read %d bytes", headerLen, n)
+		return
+	}
+
+	reader := bytes.NewReader(header)
+	binary.Read(reader, binary.LittleEndian, &m.Opcode)
+	var payloadLen int32
+	binary.Read(reader, binary.LittleEndian, &payloadLen)
+
+	payload := make([]byte, payloadLen)
+	switch n, err = conn.Read(payload); {
+	case err != nil:
+		return
+	case n != int(payloadLen):
+		return m, fmt.Errorf("Wanted %d-byte payload, read %d bytes", payloadLen, n)
+	}
+	m.Payload = string(payload[:])
+	return
+}
+
+// writeMessage writes a message to the socket.
+func writeMessage(m message, conn net.Conn) (err error) {
+	buf := m.encode()
+	var n int
+	if n, err = conn.Write(buf); n != len(buf) {
+		err = fmt.Errorf("Wanted to write %d bytes, wrote %d bytes", len(buf), n)
+	}
+	return
+}
---
+++ internal/discord/discord_test.go
@@ -0,0 +1,326 @@
+package discord
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+	"testing"
+	"time"
+)
+
+// Number of seconds to wait for things that should be near-instantaneous.
+const timeoutSeconds = 2
+
+func TestGetDiscordSocket(t *testing.T) {
+	var tests = []struct {
+		tmpDir string
+		n      int
+		want   string
+	}{
+		{"/tmp/foo", 0, "/tmp/foo/discord-ipc-0"},
+		{"/tmp/foo", 1, "/tmp/foo/discord-ipc-1"},
+		{"/tmp/bar", 0, "/tmp/bar/discord-ipc-0"},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%d", tt.tmpDir, tt.n)
+		t.Run(testname, func(t *testing.T) {
+			ans := getDiscordSocket(tt.tmpDir, tt.n)
+			if ans != tt.want {
+				t.Errorf("got %s, want %s", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestMessageEncode(t *testing.T) {
+	m := message{
+		Opcode:  1,
+		Payload: `{}`,
+	}
+	want := []byte("\x01\x00\x00\x00\x02\x00\x00\x00{}")
+	got := m.encode()
+	if !bytes.Equal(want, got) {
+		t.Errorf("Wanted %v, got %v", want, got)
+	}
+}
+
+// fakeServer creates and listens on a UNIX domain socket, like Discord would.
+type fakeServer struct {
+	TmpDir string
+	addr   string
+}
+
+func newFakeServer() (fake fakeServer, err error) {
+	fake = fakeServer{}
+
+	fake.TmpDir, err = os.MkdirTemp("", "discord_test")
+	if err != nil {
+		return fake, err
+	}
+	fake.addr = fmt.Sprintf("%s/discord-ipc-0", fake.TmpDir)
+	return fake, err
+}
+
+func (fake fakeServer) Listener() (listener net.Listener, err error) {
+	return net.Listen("unix", fake.addr)
+}
+
+func (fake fakeServer) Close() {
+	os.RemoveAll(fake.TmpDir)
+}
+
+func TestDial(t *testing.T) {
+	fake, err := newFakeServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(fake.Close)
+	listener, err := fake.Listener()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	errors := make(chan error)
+
+	serverDone := make(chan net.Conn)
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			errors <- fmt.Errorf("Got %v while waiting for connections", err)
+		}
+		serverDone <- conn
+	}()
+
+	clientDone := make(chan *Client)
+	go func() {
+		client, err := Dial(fake.TmpDir)
+		if err != nil {
+			errors <- fmt.Errorf("Got %v while dialing", err)
+		}
+		clientDone <- client
+	}()
+
+	// Wait for both the client and server goroutines to finish.
+	for n := 2; n > 0; {
+		select {
+		case err := <-errors:
+			t.Error(err)
+		case <-serverDone:
+			n--
+		case <-clientDone:
+			n--
+		case <-time.After(timeoutSeconds * time.Second):
+			t.Fatal("Timeout")
+			n = 0
+		}
+	}
+}
+
+// fakeConn is a channel-driven implementation of net.Conn for testing.
+type fakeConn struct {
+	// WriteCh is sent packets from Write().
+	WriteCh chan []byte
+	// ReadCh is sent packets to be read with Read().
+	ReadCh chan []byte
+	// readBuf contains any unread bytes from the current packet.
+	readBuf []byte
+}
+
+func NewFakeConn() *fakeConn {
+	return &fakeConn{
+		WriteCh: make(chan []byte),
+		ReadCh:  make(chan []byte),
+	}
+}
+
+func (fake *fakeConn) Read(b []byte) (n int, err error) {
+	if len(fake.readBuf) == 0 {
+		fake.readBuf = <-fake.ReadCh
+	}
+	n = copy(b, fake.readBuf)
+	fake.readBuf = fake.readBuf[n:]
+	return
+}
+
+func (fake *fakeConn) Write(b []byte) (n int, err error) {
+	fake.WriteCh <- b
+	return len(b), nil
+}
+
+func (fake *fakeConn) Close() error {
+	close(fake.WriteCh)
+	return nil
+}
+
+func (fake *fakeConn) LocalAddr() (addr net.Addr) {
+	return
+}
+
+func (fake *fakeConn) RemoteAddr() (addr net.Addr) {
+	return
+}
+
+func (fake *fakeConn) SetDeadline(t time.Time) (err error) {
+	return
+}
+
+func (fake *fakeConn) SetReadDeadline(t time.Time) (err error) {
+	return
+}
+
+func (fake *fakeConn) SetWriteDeadline(t time.Time) (err error) {
+	return
+}
+
+func TestClientSend(t *testing.T) {
+	fakeConn := NewFakeConn()
+	client := newClient(fakeConn)
+
+	startDone := make(chan error)
+	go func() {
+		startDone <- client.Start()
+	}()
+
+	type sendData struct {
+		opcode                          int32
+		requestPayload, responsePayload string
+		requestWire, responseWire       []byte
+	}
+
+	// testSend returns a test subcase function that verifies a message is
+	// sent and the appropriate answer is received.
+	testSend := func(data sendData) func(t *testing.T) {
+		done := make(chan int)
+		errors := make(chan error)
+
+		go func() {
+			ans, err := client.Send(data.requestPayload)
+
+			if err != nil {
+				errors <- err
+			}
+			if ans != data.responsePayload {
+				errors <- fmt.Errorf("Client wanted answer `%s`, got `%s`", data.responsePayload, ans)
+			}
+			done <- 1
+		}()
+
+		go func() {
+			buf := <-fakeConn.WriteCh
+
+			if !bytes.Equal(buf, data.requestWire) {
+				errors <- fmt.Errorf("Server wanted %v, got %v", data.requestWire, buf)
+			}
+
+			fakeConn.ReadCh <- data.responseWire
+			done <- 1
+		}()
+
+		return func(t *testing.T) {
+			// Wait for both the client and server goroutines to finish.
+			for n := 2; n > 0; {
+				select {
+				case err := <-errors:
+					t.Error(err)
+				case <-done:
+					n--
+				case <-time.After(timeoutSeconds * time.Second):
+					t.Fatal("Timeout")
+					n = 0
+				}
+			}
+		}
+	}
+
+	t.Run("SendHandshake", testSend(sendData{
+		opcode:          0,
+		requestPayload:  `{"cmd":"TEST"}`,
+		responsePayload: `{"cmd":"TEST","reply":1}`,
+		requestWire:     []byte("\x00\x00\x00\x00\x0e\x00\x00\x00" + `{"cmd":"TEST"}`),
+		responseWire:    []byte("\x00\x00\x00\x00\x18\x00\x00\x00" + `{"cmd":"TEST","reply":1}`),
+	}))
+
+	t.Run("SendFrame", testSend(sendData{
+		opcode:          1,
+		requestPayload:  `{"cmd":"TEST"}`,
+		responsePayload: `{"cmd":"TEST","reply":1}`,
+		requestWire:     []byte("\x01\x00\x00\x00\x0e\x00\x00\x00" + `{"cmd":"TEST"}`),
+		responseWire:    []byte("\x01\x00\x00\x00\x18\x00\x00\x00" + `{"cmd":"TEST","reply":1}`),
+	}))
+
+	t.Run("Close", func(t *testing.T) {
+		client.Close()
+		select {
+		case err := <-startDone:
+			if err != nil {
+				t.Errorf("Start() returned error %v, wanted nil", err)
+			}
+		case <-time.After(timeoutSeconds * time.Second):
+			t.Fatal("Timeout waiting for Start() to return")
+		}
+	})
+}
+
+func TestClientServerInitiated(t *testing.T) {
+	fakeConn := NewFakeConn()
+	client := newClient(fakeConn)
+
+	startDone := make(chan error)
+	go func() {
+		startDone <- client.Start()
+	}()
+
+	t.Run("Ping", func(t *testing.T) {
+		done := make(chan int)
+		errors := make(chan error)
+
+		go func() {
+			fakeConn.ReadCh <- []byte("\x03\x00\x00\x00\x0d\x00\x00\x00" + `{"nonce":"1"}`)
+			response := []byte("\x04\x00\x00\x00\x0d\x00\x00\x00" + `{"nonce":"1"}`)
+
+			buf := <-fakeConn.WriteCh
+			if !bytes.Equal(buf, response) {
+				errors <- fmt.Errorf("Server wanted %v, got %v", response, buf)
+			}
+
+			done <- 1
+		}()
+
+		select {
+		case <-done:
+			// Good.
+		case err := <-errors:
+			t.Error(err)
+		case <-time.After((1 + timeoutSeconds) * time.Second):
+			t.Fatal("Timeout")
+		}
+	})
+
+	t.Run("Close", func(t *testing.T) {
+		done := make(chan int)
+
+		go func() {
+			fakeConn.ReadCh <- []byte("\x02\x00\x00\x00\x17\x00\x00\x00" + `{"code":1,"message":""}`)
+			done <- 1
+		}()
+
+		select {
+		case <-done:
+			// Good.
+		case <-time.After(timeoutSeconds * time.Second):
+			t.Fatal("Timeout waiting for Start() to return")
+		}
+
+		select {
+		case err := <-startDone:
+			if err == nil {
+				t.Errorf("Wanted Start() to return error, got nil")
+			}
+		case <-time.After(timeoutSeconds * time.Second):
+			t.Fatal("Timeout waiting for Start() to return")
+		}
+	})
+}
